c53e3a1b0b3cb9cb1c597583603873a85e51975c
EC2 Default User <ec2-user@ip-172-31-11-58.us-east-2.compute.internal>
2018-01-26 03:46:26 +0000

    more stuff

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,13 +3,56 @@
 # Part 1
 
 def sum arr
-  sum = 0
-  arr.each{ |i| sum += i }
-  return sum
+  total = 0
+  arr.each{ |num| total += num }
+  return total
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  total = 0
+  
+  # check if the array is empty
+  if arr.empty?
+    return 0
+    
+  # return the first element if it's the only one
+  else if arr.count == 1
+    total = arr.first
+    
+  # sum the two maxes
+  else
+    
+    # assume first two elements are largest
+    val1 = arr[0]
+    val2 = arr[1]
+    index1 = 0
+    index2 = 1
+    count = 0
+    
+    # do a check for the first max
+    for num in arr do
+      if num > val1 and count != index2
+        val1 = num
+        index1 = count
+      end
+      count += 1
+    end
+    count = 0
+    
+    # check for the second max
+    for num in arr do
+      
+      # make sure it isn't the same item as the first max
+      if num > val2 and count != index1
+        val2 = num
+        index2 = count
+      end
+      count += 1
+    end
+    total = val1 + val2
+  end
+  
+  return total
 end
 
 def sum_to_n? arr, n
